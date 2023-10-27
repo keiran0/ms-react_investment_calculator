5587114594953c85f085e587e446b89e2196f065
Keiran <keinn2210@gmail.com>
2023-10-27 23:02:20 +0800

    removed components from header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import Header from './components/Header';
+import Input from './components/Input';
+import Results from './components/Results'
+
+import { useState } from 'react'
 
 function App() {
+
+  const [initial, setInitial] = useState(0);
+  const [annual, setAnnual] = useState(0);
+  const [expected, setExpected] = useState(0);
+  const [duration, setDuration] = useState(0);
+
   return (
-    
     <>
-      <Header/>
+      <Header />
+      <div id="user-input">
+        <Input labelName='Initial Investment' changeValueHandler={(e) => setInitial(Number(e.target.value))} />
+        <Input labelName='Annual Investment' changeValueHandler={(e) => setAnnual(Number(e.target.value))} />
+        <Input labelName='Expected Return' changeValueHandler={(e) => setExpected(Number(e.target.value))} />
+        <Input labelName='Duration' changeValueHandler={(e) => setDuration(Number(e.target.value))} />
+      </div>
+      <Results initial={initial} annual={annual} expected={expected} duration={duration}/>
     </>
 
 
